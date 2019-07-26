ad9bebe97e3c0740f02233c92d634fd2cc00dbfe
Twinkle Lahariya <twinkle.lahariya@synapse.com>
2019-07-26 22:21:25 +0530

    updated songlibrary

--- CoolAssignment/src/Main/Home.java
+++ CoolAssignment/src/Main/Home.java
@@ -2,7 +2,10 @@ package Main;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+//import java.util.Scanner;
 import java.util.Scanner;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 //import Songs.Album;
 import Structures.*;
@@ -13,15 +16,15 @@ public class Home {
 
 		Songs[] songData = new Songs[8];
 
-		Songs s1 = new Songs("Perfect", "xyz",LocalDate.of(2017, 1, 13));
-		Songs s2 = new Songs("Shape Of You", "pqr",LocalDate.of(2017, 1, 13));
-		Songs s3 = new Songs("Love Story", "abc",LocalDate.of(2017, 1, 13));
+		Songs s1 = new Songs("Perfect", "xyz", LocalDate.of(2017, 1, 13));
+		Songs s2 = new Songs("Shape Of You", "pqr", LocalDate.of(2017, 1, 13));
+		Songs s3 = new Songs("Love Story", "abc", LocalDate.of(2017, 1, 13));
 		Songs s4 = new Songs("You Belong With Me", "def", LocalDate.of(2017, 1, 13));
-		Songs s5 = new Songs("Channa Mereya", "Ae Dil Hai mushkil",LocalDate.of(2017, 1, 13));
-		Songs s6 = new Songs("Tujhe Kitna Chahane Lage ham", "kabir Singh",LocalDate.of(2017, 1, 13));
+		Songs s5 = new Songs("Channa Mereya", "Ae Dil Hai mushkil", LocalDate.of(2017, 1, 13));
+		Songs s6 = new Songs("Tujhe Kitna Chahane Lage ham", "kabir Singh", LocalDate.of(2017, 1, 13));
 		Songs s7 = new Songs("Numb", "ghi", LocalDate.of(2017, 1, 13));
 		Songs s8 = new Songs("In the end", "jkl", LocalDate.of(2017, 1, 13));
-		
+
 		songData[0] = s1;
 		songData[1] = s2;
 		songData[2] = s3;
@@ -34,29 +37,33 @@ public class Home {
 		return songData;
 	}
 
-	public static void showAllLibraries(ArrayList<Object> library) {
-
+	public static void showAllLibraries(ArrayList<Library> library) {
+			
+		for(int i=0;i<2;i++)
+		System.out.println((library.get(i)).getLibrary());
 	}
-
-	public static void showAllAlbums(ArrayList<Object> album) {
-
-	}
-
-	public static void showAllSongs(ArrayList<Object> songs) {
-
-	}
-
-	public static void showLibrary(ArrayList<Object> library) {
-
-	}
-
-	public static void showAlbum(ArrayList<Object> album) {
-
-	}
-
-	public static void showSong(ArrayList<Object> songs) {
-
-	}
+////
+//	static void showAllAlbums(ArrayList<Library> libList) {
+//		for(int i = 0; i < 3; i++) {
+//				libList.get(i).displayAlbums();
+//		}
+//	}
+////
+//	public static void showAllSongs(ArrayList<Library> library) {
+//
+//	}
+//
+//	public static void showLibrary(ArrayList<Object> library) {
+//
+//	}
+//
+//	public static void showAlbum(ArrayList<Object> album) {
+//
+//	}
+//
+//	public static void showSong(ArrayList<Object> songs) {
+//
+//	}
 
 	public static ArrayList<Library> createData() {
 
@@ -69,41 +76,57 @@ public class Home {
 	public static ArrayList<Album> albumValues(Songs[] songData) {
 		ArrayList<Album> result = new ArrayList<>();
 		// TODO Auto-generated method stub
-		for (int i = 0; i < 8; i ++) {
-			Album a1 = new Album("Album" + (i+1));
+		for (int i = 0; i < 8; i++) {
+			Album a1 = new Album("Album" + (i + 1));
 			a1.songs.add(songData[i]);
 			i += 1;
 			a1.songs.add(songData[i]);
 			result.add(a1);
-		}	
-		
+		}
+
 		return result;
 	}
-	
+
 	public static ArrayList<Library> libraryValues(ArrayList<Album> albums) {
 		ArrayList<Library> result = new ArrayList<>();
 		// TODO Auto-generated method stub
-		for (int i = 0; i < 4; i ++) {
+		for (int i = 0; i < 4; i++) {
 			Library l = new Library("Library" + (i + 1));
 			l.albums.add(albums.get(i));
 			i++;
 			l.albums.add(albums.get(i));
 			result.add(l);
-		}	
-		
+		}
+
 		return result;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-//		System.out.println("Enter Size of Library");
-//		@SuppressWarnings("resource")
-//		Scanner sr = new Scanner(System.in);
-//		final int number = sr.nextInt();
-		createData();
-		System.out.println();
+		System.out.println("00:All Libraries\n01:Library 1\n02:Library 2\nEnter your choice::");
+
+		ArrayList<Library> library = createData();
+
+		Scanner sc = new Scanner(System.in); // creating and initializing the Scanner class for getting user data
+
+		String choice = sc.nextLine(); // taking user choice
+
+		switch (choice) {
+		case "00":
+			showAllLibraries(library);
+			break;
+		case "01":
+			System.out.println(library.get(0));
+			break;
+		case "02":
+			System.out.println(library.get(1));
+			break;
+		
+		default:
+			System.out.println("Invalid Choice,try again");
+			break;
+		}
 
 	}
-
 }
--- CoolAssignment/src/Structures/Start.java
+++ CoolAssignment/src/Structures/Start.java
@@ -1,22 +1,22 @@
-package Structures;
-
-import java.util.ArrayList;
-
-public class Start implements Comparable{
-	
-	ArrayList<Library> library = new ArrayList<Library>();
-	
-	
-	
-	
-	void createData() {
-		
-	}
-
-	@Override
-	public int compareTo(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
-	} 	
-	
-}
+//package Structures;
+//
+//import java.util.ArrayList;
+//
+//public class Start implements Comparable{
+//	
+//	ArrayList<Library> library = new ArrayList<Library>();
+//	
+//	
+//	
+//	
+//	void createData() {
+//		
+//	}
+//
+//	@Override
+//	public int compareTo(Object o) {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	} 	
+//	
+//}
